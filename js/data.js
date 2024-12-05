d7e0d47d1cae24d2a4eaf3cb9822d7ab25168a87
R. Sam Craig <SAMCRAIG70@GMAIL.COM>
2024-12-04 18:48:04 -0800

    Removed all style keys

--- js/data.js
+++ js/data.js
@@ -7,210 +7,159 @@ export const imgArray = [
     {
         src: './imgs/img2.jpg',
         desc: '2',
-        id: crypto.randomUUID(),
-        style: ''
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img3.jpg',
         desc: '3',
-        people: '',
-        id: crypto.randomUUID(),
-        
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img4.jpg',
         desc: '4',
-        people: '',
-        id: crypto.randomUUID(),
-        style: ''
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img5.jpg',
         desc: '5',
-        people: '',
-        id: crypto.randomUUID(),
-        year : '111',
-        style: 'wide'
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img6.jpg',
         desc: '6',
-        people: '',
-        id: crypto.randomUUID(),
-        style: 'wide'
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img7.jpg',
         desc: '7',
-        people: '',
-        id: crypto.randomUUID(),
-        year: 1,
-        style: 'wide'
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img8.jpg',
         desc: '8',
-        people: '',
-        id: crypto.randomUUID(),
-        style: ''
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img9.jpg',
         desc: '9',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img10.jpg',
         desc: '10',
-        people: '',
         id: crypto.randomUUID(),
-        style: 'wide'
+       
     },{
         src: './imgs/img1.jpg',
         desc: '1',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img2.jpg',
         desc: '2',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img3.jpg',
         desc: '3',
-        people: '',
         id: crypto.randomUUID(),
         
     },
     {
         src: './imgs/img4.jpg',
         desc: '4',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img5.jpg',
         desc: '5',
-        people: '',
         id: crypto.randomUUID(),
-        year : '111',
-        style: 'wide'
+       
     },
     {
         src: './imgs/img6.jpg',
         desc: '6',
-        people: '',
         id: crypto.randomUUID(),
-        style: 'wide'
+       
     },
     {
         src: './imgs/img7.jpg',
         desc: '7',
-        people: '',
         id: crypto.randomUUID(),
-        year: 1,
-        style: 'wide'
+       
     },
     {
         src: './imgs/img8.jpg',
         desc: '8',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img9.jpg',
         desc: '9',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img10.jpg',
         desc: '10',
-        people: '',
         id: crypto.randomUUID(),
-        style: 'wide'
+       
     },{
         src: './imgs/img1.jpg',
         desc: '1',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img2.jpg',
         desc: '2',
-        people: '',
         id: crypto.randomUUID(),
-        style: ''
+       
     },
     {
         src: './imgs/img3.jpg',
         desc: '3',
-        people: '',
         id: crypto.randomUUID(),
         
     },
     {
         src: './imgs/img4.jpg',
         desc: '4',
-        people: '',
-        id: crypto.randomUUID(),
-        style: ''
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img5.jpg',
         desc: '5',
-        people: '',
-        id: crypto.randomUUID(),
-        year : '111',
-        style: 'wide'
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img6.jpg',
         desc: '6',
-        people: '',
-        id: crypto.randomUUID(),
-        style: 'wide'
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img7.jpg',
         desc: '7',
-        people: '',
-        id: crypto.randomUUID(),
-        year: 1,
-        style: 'wide'
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img8.jpg',
         desc: '8',
-        people: '',
-        id: crypto.randomUUID(),
-        style: ''
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img9.jpg',
         desc: '9',
-        people: '',
-        id: crypto.randomUUID(),
-        style: ''
+        id: crypto.randomUUID()
     },
     {
         src: './imgs/img10.jpg',
         desc: '10',
-        people: '',
-        id: crypto.randomUUID(),
-        style: 'wide'
+        id: crypto.randomUUID()
     },
-   
-
 ]
-
